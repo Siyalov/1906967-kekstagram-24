efb95ff51141f932b3b0942f0b15ce345c0341ba
Siyalov <91375207+Siyalov@users.noreply.github.com>
2021-11-05 19:24:10 +0300

    Update main.js
    main js function add

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function getRandomInt(min, max) {
+  if (min > max) 
+    {
+      alert('не правильный диапазон, отдаем число 0');
+      return 0;
+    }
+  
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
+function checkMaxLenght(str, length) {
+    if (str.length >= length) {
+      return true;
+      }
+    return false;
+}
