f60d41eca0532c98e49f5437f442b1f63a1ca3e0
Siyalov <91375207+Siyalov@users.noreply.github.com>
2021-11-16 17:43:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,10 +18,6 @@ function checkMaxLenght(str, length) {
     return false;
 }
 
-function checkRandomComment(num) {
-  
-}
-
 let fotoInfo = {
   'id': 0, // foto name.jpg
   'comments': 0,
@@ -36,21 +32,36 @@ let comments = {
   name: '',
 }
 
-
+let arrUserComment = [];
 
 function makeArray() {
   let i = 1;
   let arrFoto = [];
-  let arrUserCommment = [];
   let comments = ["Всё отлично!", "В целом всё неплохо. Но не всё.", "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. ","Моя бабушка случайно чихнула","Я поскользнулся на банановой кожуре","Лица у людей на фотке перекошены"];
   let discr = ["пейзаж", "натюрмотр","портрет"];
   
   while (i < 26) { // выводит 0, затем 1, затем 2
     i++;    
     arrUserCommment = getRandomInt(0,5)
-    arrFoto[i] = new fotoInfo(i,getRandomInt(0,5),getRandomInt(14,200),getRandomInt(0,2));
+    arrFoto[i] = new fotoInfo(i,checkRandomComment(),getRandomInt(14,200),discr[getRandomInt(0,2)]);
     
     }
 }
 
 
+function checkRandomComment() {
+  let comments = ["Всё отлично!", "В целом всё неплохо. Но не всё.", "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. ","Моя бабушка случайно чихнула","Я поскользнулся на банановой кожуре","Лица у людей на фотке перекошены"];
+  let name = ["Karl", "Pepe"];  
+  let flag = true
+  while(flag) {
+    nameId = getRandomInt(0,1000)
+    for (let i = 0; i < comments.length; i += 1) {
+      if (comments[i].id == nameId){
+         flag = true
+        }
+      }
+    flag = false
+  }
+  newCommetn = new comments(nameId, getRandomInt(1,7), comments[getRandomInt(0,5)], name[getRandomInt(0,1)]);
+  arrUserComment.push()
+}
